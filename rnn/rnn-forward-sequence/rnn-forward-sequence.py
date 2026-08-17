a3e71e20e26ac5ceae46429f7ae1cef30e637eb1
irvin sachdeva <149496910+1rvinn@users.noreply.github.com>
2026-08-17 14:40:03 +0530

    Solve rnn-forward-sequence

--- rnn/rnn-forward-sequence/rnn-forward-sequence.py
+++ rnn/rnn-forward-sequence/rnn-forward-sequence.py
@@ -5,12 +5,12 @@ def rnn_forward(X: np.ndarray, h_0: np.ndarray,
     """
     Forward pass through entire sequence.
     """
-    T = X.shape[1]
-    batch = X.shape[0]
-    hidden_dim = h_0.shape[1]
-    
-    hidden_states = np.zeros((batch,T,hidden_dim))
-    hidden_states[:,-1,:]=h_0
-    for t in range(0,T):
-        hidden_states[:,t,:]=np.tanh(np.matmul(X[:,t,:], W_xh.transpose()) + np.matmul(hidden_states[:,t-1,:], W_hh.transpose())+b_h)
-    return (hidden_states, hidden_states[:,-1,:])
+    batch=X.shape[0]
+    T=X.shape[1]
+    hidden_dim=h_0.shape[-1]
+    hidden_states = np.zeros((batch, T, hidden_dim))
+    h_prev=h_0
+    for i in range(T):
+        hidden_states[:,i,:]=np.tanh(np.matmul(X[:,i,:],W_xh.T)+np.matmul(h_prev,W_hh.T)+b_h)
+        h_prev=hidden_states[:,i,:]
+    return (hidden_states, h_prev)
